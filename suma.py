8378cc092208cabcda2c6f52ab7a461b89aa79e3
Ricardo Rodríguez <146163510+RicardoRdz21@users.noreply.github.com>
2026-03-12 08:18:50 -0600

    Update test.py

--- suma.py
+++ suma.py
@@ -1,9 +1,3 @@
 import sys
-
-def sumar_argumentos(n1, n2):
-    resultado = int(n1) + int(n2)
-    print(resultado)
-
-if __name__ == "__main__":
-    # Recibimos los números desde el script de Bash
-    sumar_argumentos(sys.argv[1], sys.argv[2])
+# Solo imprimimos el número, sin texto adicional
+print(int(sys.argv[1]) + int(sys.argv[2]))
